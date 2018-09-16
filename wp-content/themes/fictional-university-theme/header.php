38aedf8b001d07c6ccc5142a32de918c141395a9
Giselle Tavares <gitavares@hotmail.com>
2018-09-15 21:31:35 -0400

    relationship between pages

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -20,7 +20,7 @@
             ?>
             <ul>
                 <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 11) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/index.php/about-us');?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
+                <li <?php if(get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program') ?>">Programs</a></li>
                 <li <?php if(get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event')?>">Events</a></li>
                 <li><a href="#">Campuses</a></li>
                 <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/index.php/blog'); ?>">Blog</a></li>
