058adcc3d552e85ec06e4296be91dd07aa1fb1c9
Giselle Tavares <gitavares@hotmail.com>
2018-09-16 23:04:20 -0400

    new manual plugins and installed some plugins

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -43,6 +43,24 @@
             ),
             'menu_icon' => 'dashicons-awards'
         ));
+
+        // Professors Post Type
+        register_post_type('professor', array(
+            'supports' => array(
+                'title', 
+                'editor',
+                'thumbnail'
+            ),
+            'public' => true,
+            'labels' => array(
+                'name' => 'Professors',
+                'add_new_item' => 'Add New Professor',
+                'edit_item' => 'Edit Professor',
+                'all_items' => 'All Professors',
+                'singular_name' => 'Professor'
+            ),
+            'menu_icon' => 'dashicons-welcome-learn-more'
+        ));
     }
 
     add_action('init', 'university_post_types');
