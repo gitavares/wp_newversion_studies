d7d5700d2cf1767c34ba41344789f0180ce7fb9a
Giselle Tavares <gitavares@hotmail.com>
2018-09-27 16:09:33 -0400

    Like feature implemented

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -108,6 +108,23 @@
             ),
             'menu_icon' => 'dashicons-welcome-write-blog'
         ));
+        
+        // Like Post Type
+        register_post_type('like', array(
+            'supports' => array(
+                'title'
+            ),
+            'public' => false,
+            'show_ui' => true, //show in admin dashboard
+            'labels' => array(
+                'name' => 'Likes',
+                'add_new_item' => 'Add New Like',
+                'edit_item' => 'Edit Like',
+                'all_items' => 'All Likes',
+                'singular_name' => 'Like'
+            ),
+            'menu_icon' => 'dashicons-heart'
+        ));
     }
 
     add_action('init', 'university_post_types');
